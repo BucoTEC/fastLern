91f71bf940adc59caa2f7d2fd7bdbe6a08f7de37
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-22 00:28:02 +0100

    add basic path param

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import home, room
 
 urlpatterns =[
-    path('home/',home, name="home" ),
-    path('room/',room, name="room" )
+    path('',home, name="home" ),
+    path('room/<str:pk>',room, name="room" )
 
 ]
