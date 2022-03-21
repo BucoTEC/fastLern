cdf53b181393349dd9200470a6e77513276fe160
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-22 00:18:23 +0100

    add basic data context

--- base/views.py
+++ base/views.py
@@ -1,10 +1,22 @@
+from multiprocessing import context
 from django.shortcuts import render
 # from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id':1,'name':'python'},
+    {'id':2,'name':'javaScript'},
+    {'id':3,'name':'goLang'},
+
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context ={
+        'rooms':rooms
+    }
+    return render(request, 'home.html', context)
 
 def room(request):
-    return render(request, 'room.html')
+    
+    return render(request, 'room.html', )
