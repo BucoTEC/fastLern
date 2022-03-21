78d140daab31996fbe83b95dc3b1a493732c2c71
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-22 00:20:26 +0100

    add basic teplates directory in app

--- base/views.py
+++ base/views.py
@@ -15,8 +15,8 @@ def home(request):
     context ={
         'rooms':rooms
     }
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 def room(request):
     
-    return render(request, 'room.html', )
+    return render(request, 'base/room.html', )
