b0711c336d4957c5b6605ccdd7f912a7681c5ecb
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-23 18:24:49 +0100

    add update functionaliti

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import home, room, createRoom
+from .views import home, room, createRoom, updateRoom
 
 urlpatterns =[
     path('',home, name="home" ),
     path('room/<str:pk>',room, name="room" ),
     path('create-room/',createRoom, name="create-room" ),
+    path('update-room/<str:pk>',updateRoom, name="update-room" ),
+
 ]
